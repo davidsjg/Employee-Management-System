175064e6530c1362945e034d685105bff40804a8
Joey Davidson <j.davidson18@gmail.com>
2021-02-24 09:28:48 -0700

    join of 3 tables functioning

--- employee.js
+++ employee.js
@@ -1,7 +1,8 @@
 const mysql = require('mysql')
 const inquirer = require('inquirer')
+require ('console.table')
 
-//need to create a connection configuration (passing in an object)
+//need to create a connection configuration (passing in an object
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
@@ -17,25 +18,33 @@ const start = () => {
         type: "list",
         message: "What would you like to do?",
         default: "(use arrow keys)",
-        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employees", "Add Departments", "Add Roles", "Update Employee Roles"]
+        choices: ["View All Employees", "View All Employees by Department", "View All Employees by Manager", "Add Employees", "Add Department", "Add Role", "Update Employee Roles", "Quit"]
     }).then((answer) => {
         if (answer.mainAction == "View All Employees"){
             getEmployees()
+        }
+        if (answer.mainAction == "View All Departments"){
+            getEmployeesDept()
+        }
+        if (answer.mainAction == "View All Roles"){
+            getEmployeesDept()
+        }
+        if (answer.mainAction == "Add Employees"){
+            addEmployee()
+        }
+        if (answer.mainAction == "Add Department"){
+            addDepartment()
+        }
+        if (answer.mainAction == "Add Role"){
+            addRole()
+        }
+        else {
+            updateEmployeeRole()
         }
         
 
     })
 }
-
-// * What would you like to do? (use arrow keys)
-//     * View All Employees 
-//     * View All Employees by Department 
-//     * View All Employees by Manager 
-//     * Add Employees
-//     * Add Departments
-//     * Add Roles
-//     * Update Employee Roles
-
 
 //storing the value of the callback function within getData inside of getData
 const getEmployees = ()=> {
@@ -43,7 +52,7 @@ const getEmployees = ()=> {
     //interacting with a database is asynchronous
     connection.query('SELECT * FROM employee', (err, data) => {
         if (err) throw err
-        console.log(data)
+        console.table(data)
     })
 
 }
--- employeeSeeds.sql
+++ employeeSeeds.sql
@@ -2,17 +2,60 @@ DROP DATABASE IF EXISTS employees_DB;
 CREATE DATABASE employees_DB;
 USE employees_DB;
 
+
+
 CREATE TABLE employee(
-  id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id VARCHAR(20),
-  manager_id INTEGER,
-  PRIMARY KEY (id)
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    -- foreign key - pointer to something in another table --
+    role_id VARCHAR(20),
+    manager_id INTEGER,
+    PRIMARY KEY (id)
 );
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("JOEY", "DAVIDSON", 1, 5);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("TOM", "NOSDIVAD", 2, 5);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("MIKE", "HUGHES", 3, 5);
+CREATE TABLE roles (
+	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary INTEGER,
+    -- foreign key - pointer to something in another table --
+    department_id INTEGER(11),
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+	id INTEGER(11) AUTO_INCREMENT NOT NULL,
+    department VARCHAR (50),
+	PRIMARY KEY (id)
+);
+
+INSERT INTO department (department) VALUES ("Sales");
+INSERT INTO department (department) VALUES ("Legal");
+INSERT INTO department (department) VALUES ("Finance");
+
+INSERT INTO roles (title, salary, department_id) VALUES ("Sales Lead", 150000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ("Salesperson", 100000, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ("Accountant", 50000, 3);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ("JOE", "SAKIC", 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("ADAM", "DEADMARSH", 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("PETER", "FORSBERG", 3, 1);
 
 SELECT * FROM employee;
+SELECT * FROM roles;
+SELECT * FROM department;
+
+SELECT title, department, salary
+FROM roles 
+INNER JOIN department ON roles.department_id = department.id;
+
+-- INNER JOIN employee --> cant do that bc SELECT (table header) not correct
+
+
+SELECT employee.id, employee.first_name, employee.last_name, title, department, salary, employee.manager_id, CONCAT(manager.first_name, " ", manager.last_name) as manager
+FROM roles
+INNER JOIN department ON roles.department_id = department.id
+INNER JOIN employee ON employee.role_id = roles.id
+LEFT JOIN employee manager ON manager.id = employee.manager_id;
+
+
